758fabb14ea675e965d780454a808c43c612249d
ppj <prasanna_3003@yahoo.com>
2014-09-11 10:28:08 +1000

    after running 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911000659) do
+ActiveRecord::Schema.define(version: 20140911001822) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
