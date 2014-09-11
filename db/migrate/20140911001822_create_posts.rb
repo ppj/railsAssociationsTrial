446360f911c883853002cec08c2e32d5af323142
ppj <prasanna_3003@yahoo.com>
2014-09-11 10:21:09 +1000

    added comment

--- db/migrate/20140911001822_create_posts.rb
+++ db/migrate/20140911001822_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :user, index: true
+      t.references :user, index: true  # this line adds an integer column called `user_id`
 
       t.timestamps
     end
