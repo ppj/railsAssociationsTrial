4d5466712fbed74f7a238dc7d25db2f369bdafb4
ppj <prasanna_3003@yahoo.com>
2014-09-11 15:53:03 +1000

    can delete posts; destroying user destroys his/her posts too

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,13 @@ class PostsController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @post.destroy
+    redirect_to user_path(@user)
+  end
+
 
   private
 
