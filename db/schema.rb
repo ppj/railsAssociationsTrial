03cbee6826da3b140a52f6d4b8dea54b43130412
ppj <prasanna_3003@yahoo.com>
2014-09-12 23:33:34 +1000

    create the memberships table. After 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911001822) do
+ActiveRecord::Schema.define(version: 20140912050455) do
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "memberships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "memberships", ["group_id"], name: "index_memberships_on_group_id"
+  add_index "memberships", ["user_id", "group_id"], name: "index_memberships_on_user_id_and_group_id", unique: true
+  add_index "memberships", ["user_id"], name: "index_memberships_on_user_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
