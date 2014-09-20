4e74f416e682bcf02c7951ece48c981eb5e12c2d
ppj <prasanna_3003@yahoo.com>
2014-09-20 14:04:40 +1000

    models id should be the foreign key for the membership table

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ActiveRecord::Base
-  has_many :memberships, foreign_key: :user_id, dependent: :destroy
+  has_many :memberships, foreign_key: :group_id, dependent: :destroy
   has_many :users, through: :memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-  has_many :memberships, foreign_key: :group_id, dependent: :destroy
+  has_many :memberships, foreign_key: :user_id, dependent: :destroy
   has_many :groups, through: :memberships
 end
